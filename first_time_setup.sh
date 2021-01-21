1b86a7416b95beab68844ecedc31684727357ad9
Sakaeo <43930175+Sakaeo@users.noreply.github.com>
2021-01-21 14:47:52 +0100

    Update first_time_setup.sh

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -10,7 +10,8 @@ sudo apt-get install libhdf5-serial-dev
 sudo apt-get install libatlas-base-dev  
 sudo apt-get install libjasper-dev  
 sudo apt-get install libqtgui4  
-sudo apt-get install libqt4-test 
+sudo apt-get install libqt4-test
+sudo apt-get install cmake
 
 sudo pip3 install -r requirements.txt
 
